c26bcd9eb6e8d7006cd80c21297639b7d0808101
WilmerRv <wilmer8981@hotmail.com>
2026-01-12 18:20:23 -0600

    sección de comentarios y opciones de filtrado implementados

---
+++ src/PortafolioBlog/Models/CounterResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace PortafolioBlog.Models
+{
+    public class ContadorResponse
+    {
+        [JsonPropertyName("count")]
+        public long Value { get; set; }
+    }
+}
---
+++ src/PortafolioBlog/wwwroot/scripts/coments_Giscus.js
@@ -0,0 +1,26 @@
+﻿// Esta función será llamada desde Blazor cada vez que se entra a un post
+window.renderGiscus = () => {
+    const container = document.getElementById('giscus-container');
+    if (!container) return;
+
+    // se limpia el contenedor por si se viene desde otro post
+    container.innerHTML = '';
+
+    const script = document.createElement('script');
+    script.src = "https://giscus.app/client.js";
+    script.setAttribute('data-repo', "WilmerRv1989/WilmerRv1989.github.io");
+    script.setAttribute('data-repo-id', "R_kgDOQ0TQMA");
+    script.setAttribute('data-category', "General");
+    script.setAttribute('data-category-id', "DIC_kwDOQ0TQMM4C04Rn");
+    script.setAttribute('data-mapping', "pathname"); // Clave para que cada post sea único
+    script.setAttribute('data-strict', "0");
+    script.setAttribute('data-reactions-enabled', "1");
+    script.setAttribute('data-emit-metadata', "0");
+    script.setAttribute('data-input-position', "top");
+    script.setAttribute('data-theme', "dark"); // Tema oscuro para tu blog
+    script.setAttribute('data-lang', "es");
+    script.setAttribute('crossorigin', "anonymous");
+    script.async = true;
+
+    container.appendChild(script);
+};
